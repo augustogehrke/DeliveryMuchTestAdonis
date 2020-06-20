b3795dd64ec6e4cc317c9cff618bfe8ae21cd961
Augusto Gehrke <augustogehrke@outlook.com>
2020-06-20 11:02:46 -0300

    Conclusão da criação do endpoint recipes [GET]. Foi realizado a implementação das seguintes funcionalidades: 1-obtenção do gif através da api giphy; 2-formatação em array e ordem alfabética dos ingredientes recebidos da api Recipe Puppy; 3-retorno da requisição conforme a estrutura solicitada

--- app/Controllers/Http/RecipeController.js
+++ app/Controllers/Http/RecipeController.js
@@ -1,12 +1,54 @@
 'use strict'
 
 const RecipePuppyApi = use('Services/RecipePuppyApi')
+const GiphyApi = use('Services/GiphyApi')
 
 class RecipeController {
   async index ({ request }) {
     const ingredients = request.input('i')
-    const data = await RecipePuppyApi.getRecipes(ingredients)
-    return data
+    const recipes = await this.getRecipes(ingredients)
+    return recipes
+  }
+
+  async getRecipes (params) {
+    const { results } = await RecipePuppyApi.getRecipes(params)
+    const recipes = await this.formatRecipes(results)
+
+    const keywords = params.split(',')
+    return { keywords, recipes }
+  }
+
+  async formatRecipes (recipes) {
+    const listRecipes = []
+
+    for (const recipe of recipes) {
+      const gif = await this.getGif(recipe.title)
+      const ingredients = await this.formatIngredients(recipe.ingredients)
+
+      listRecipes.push({
+        title: recipe.title,
+        ingredients,
+        link: recipe.href,
+        gif
+      })
+    }
+
+    return listRecipes
+  }
+
+  async getGif (title) {
+    const { data } = await GiphyApi.searchGif(title)
+    if (Array.isArray(data) && data.length > 0) {
+      return data[0].images.original.url
+    }
+
+    return null
+  }
+
+  async formatIngredients (ingredientsText) {
+    const ingredientsList = ingredientsText.split(',')
+    const ingredients = ingredientsList.map(ingredient => ingredient.trim())
+    return ingredients.sort()
   }
 }
 
